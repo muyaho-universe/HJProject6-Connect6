5e2fedfd08e10af0854bff2c04a83265f07eb16d
muyaho-universe <kimdaesk@gmail.com>
2022-07-04 13:10:00 +0900

    Do the initial commit

---
+++ app/src/main/java/com/dale/connect6/MainFrame.java
@@ -0,0 +1,12 @@
+package com.dale.connect6;
+
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
